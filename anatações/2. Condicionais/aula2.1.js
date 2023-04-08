2abe2dc6109ad8a16a9d9e5d121e4ccbcc8869db
Edison Manzzolla Junior <91670433+manzzolla@users.noreply.github.com>
2023-04-08 00:03:49 +0000

    novo arquivo de aula

---
+++ anatações/2. Condicionais/aula2.1.js
@@ -0,0 +1,15 @@
+
+
+const numero = 5;
+const numeroDivisivelPo5 = (numero % 5) === 0;
+
+
+if (numero === 0) { 
+    console.log('O número é invalido');
+} else if (numeroDivisivelPo5) { 
+    console.log('sim');
+}    else { 
+        console.log('não');
+    }
+
+    
